330ae48bd8651663a17850b8efb835e2fd572549
최선하 <choish_22@thingspire.com>
2025-08-07 21:26:04 +0900

    initial commit

---
+++ src/main/java/com/choiseonha/choiseonha/ChoiseonhaApplication.java
@@ -0,0 +1,13 @@
+package com.choiseonha.choiseonha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoiseonhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoiseonhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/choiseonha/choiseonha/ChoiseonhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choiseonha.choiseonha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoiseonhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
